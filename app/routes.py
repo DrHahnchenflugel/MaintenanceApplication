84c29c6db39195af9fe3b2e0312b1ae093aa9bb8
drhahnchenflugel <DrHahnchenflugel@yandex.ru>
2025-09-02 21:21:48 -0400

    new issue?

--- app/routes.py
+++ app/routes.py
@@ -92,7 +92,7 @@ def new_issue_for_asset(asset_uuid):
 
 @bp.post("/issues/new/<uuid:asset_uuid>")
 def create_issue_for_asset(asset_uuid):
-    asset = query_one("select asset_id from asset where uuid = %s", (str(asset_uuid),))
+    asset = query_one("select friendly_tag, site_id, make, model, variant, status from asset where uuid = %s", (str(asset_uuid),))
     if asset is None:
         abort(404)
     asset_id = asset[0]
@@ -109,7 +109,7 @@ def create_issue_for_asset(asset_uuid):
 
         # Re-render with entered values
         asset = query_one("""
-            select asset_id where from asset where uuid = %s
+            select friendly_tag, site_id, make, model, variant, status where from asset where uuid = %s
             """, (str(asset_uuid),))
         return render_template(f"issues/new/{asset_uuid}.html", asset=asset, form=request.form)
 
