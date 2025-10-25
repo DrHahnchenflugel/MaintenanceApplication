b4241871eb69735402bce709b5b9cba82cb54abf
DrHahnchenflugel <69130571+DrHahnchenflugel@users.noreply.github.com>
2025-10-25 00:17:43 -0400

    Refactoring for v2 db

---
+++ app/routes/__init__.py
@@ -0,0 +1,6 @@
+from .web import bp as web_bp
+from .api.v2 import bp as api_v2_bp
+
+def register_blueprints(app):
+    app.register_blueprint(web_bp)
+    app.register_blueprint(api_v2_bp) # API v2
---
+++ app/routes/web/__init__.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+bp = Blueprint("app", __name__)
+
+# Import route modules, so they attach to bp
+from . import dashboard     # noqa: F401
+from . import issues        # noqa: F401
+from . import files         # noqa: F401
+from . import assets        # noqa: F401
