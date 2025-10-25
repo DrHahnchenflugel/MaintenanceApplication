0ec992f79ad69a4796ba8a4b15291cd7830427a8
DrHahnchenflugel <69130571+DrHahnchenflugel@users.noreply.github.com>
2025-10-25 00:40:03 -0400

    Refactoring for v2 db

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,6 @@ from .web import bp as web_bp
 from .api.v2 import bp as api_v2_bp
 
 def register_blueprints(app):
-    app.register_blueprint(web_bp)
-    app.register_blueprint(api_v2_bp) # API v2
+    app.register_blueprint(web_bp, url_prefix="/maintenance")
+    app.register_blueprint(api_v2_bp, url_prefix="/maintenance") # API v2
     # keep , url_prefix="/maintenance" in both register_blueprint()s?
