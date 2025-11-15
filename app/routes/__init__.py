1325ac6558e3d54b313e5606de049e97d2a1978d
DrHahnchenflugel <69130571+DrHahnchenflugel@users.noreply.github.com>
2025-11-15 17:57:53 -0500

    Starting work on v2 api endpoints (bare ground laid out)

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,8 @@
 from .web import web_bp as web_bp
 from .api.v2 import api_v2_bp as api_v2_bp
 
+pass
+
 def register_blueprints(app):
     app.register_blueprint(web_bp, url_prefix="/maintenance")
     app.register_blueprint(api_v2_bp, url_prefix="/maintenance/api/v2") # API v2
