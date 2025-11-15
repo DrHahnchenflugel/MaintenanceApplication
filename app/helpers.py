82ce3b22bf4677211693b85b9c8b065a95bb44c9
DrHahnchenflugel <69130571+DrHahnchenflugel@users.noreply.github.com>
2025-11-15 01:12:18 -0500

    work on v2

--- app/helpers.py
+++ app/helpers.py
@@ -95,6 +95,6 @@ def timezone_to_ddmonthyyyy_hhmm(dt_utc: datetime, tz:timezone = TZ_MINUS_5) -> 
     if dt_utc is not None:
         # Normalize to target timezone
         dt_local = dt_utc.astimezone(tz)
-        return f"{dt_local.day} {dt_local.strftime("%B")} {dt_local.year} ({dt_local.hour}:{dt_local.minute})"
+        return f"{dt_local.day} {dt_local.strftime('%B')} {dt_local.year} ({dt_local.hour}:{dt_local.minute})"
     else:
         return None
