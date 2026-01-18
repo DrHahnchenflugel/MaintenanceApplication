d4652843f500556f21b22f78f00f629c09eb2ca5
DrHahnchenflugel <69130571+DrHahnchenflugel@users.noreply.github.com>
2026-01-17 22:29:25 -0500

    allow heic files

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask
 from .routes import register_blueprints
 from .health import health_bp
+from pillow_heif import register_heif_opener
+register_heif_opener()
+
 
 def initialise_application():
     from dotenv import load_dotenv
