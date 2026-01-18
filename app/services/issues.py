ae594fa5b7653e201337d13850da54752ff86a18
DrHahnchenflugel <69130571+DrHahnchenflugel@users.noreply.github.com>
2026-01-17 22:11:07 -0500

    creating new issue

--- app/services/issues.py
+++ app/services/issues.py
@@ -220,10 +220,10 @@ def create_issue(data: dict):
     )
     issue_id = issue_row["id"]
 
-    # 2) status history (NULL -> OPEN)
+    # 2) status history (OPEN -> OPEN)
     issue_db.create_issue_status_history_row(
         issue_id=issue_id,
-        from_status_id=None,
+        from_status_id=status_id,
         to_status_id=status_id,
         changed_by=created_by,  # string
     )
